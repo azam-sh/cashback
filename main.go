c388748aac1af9c16e9805010dc0a8962a1eddd2
Azam <azam.sh3603@gmail.com>
2021-07-14 19:40:05 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+const fullPercent = 100
+const cashbackPercent = 15
+const limit = 2_000
+
+func calculateCashback(amount int) {
+	cashback := 0
+	if amount > 5_000 {
+		cashback = amount * cashbackPercent / fullPercent
+	} else if amount <= 5_000 {
+		cashback = 0
+	}
+	if cashback > limit {
+		cashback = limit
+	}
+	fmt.Println(cashback)
+}
+
+func main() {
+	calculateCashback(12_000)
+}
+
+// HOMETASK
